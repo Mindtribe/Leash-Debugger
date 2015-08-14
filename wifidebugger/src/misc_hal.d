85af7cca46728fffbff12fb6aa4f7d5be4a10c30
Sander Vocke <sander@mindtribe.com>
2015-08-14 10:39:41 -0700

    Improved makefile of wifidebugger to separate .d and .o files, better folder structure.

--- wifidebugger/src/misc_hal.d
+++
@@ -1,3 +0,0 @@
-src/misc_hal.o: src/misc_hal.c ../cc3200-sdk/driverlib/rom_map.h \
- ../cc3200-sdk/driverlib/rom_patch.h ../cc3200-sdk/driverlib/utils.h \
- inc/misc_hal.h
--- wifidebugger/src/vendor/rom_pin_mux_config.d
+++
@@ -1,6 +0,0 @@
-src/vendor/rom_pin_mux_config.o: src/vendor/rom_pin_mux_config.c \
- inc/vendor/pin_mux_config.h ../cc3200-sdk/inc/hw_types.h \
- ../cc3200-sdk/inc/hw_memmap.h ../cc3200-sdk/inc/hw_gpio.h \
- ../cc3200-sdk/driverlib/pin.h ../cc3200-sdk/driverlib/gpio.h \
- ../cc3200-sdk/driverlib/prcm.h ../cc3200-sdk/driverlib/rom.h \
- ../cc3200-sdk/driverlib/rom_map.h ../cc3200-sdk/driverlib/rom_patch.h
