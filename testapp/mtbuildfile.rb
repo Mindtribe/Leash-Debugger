1dc45ec34de72a081958177b6e24549f2c5dbda1
Sander Vocke <sander@mindtribe.com>
2015-08-18 09:05:28 -0700

    git technicalities

---
+++ testapp/mtbuildfile.rb
@@ -0,0 +1,15 @@
+application_project :testapp, File.dirname(__FILE__) do |app|
+
+  app.add_configuration :Debug,
+    sources: ['src/*.c'],
+    toolchain: toolchain(:arm_none_eabi_gcc,
+      cppflags: "-Dgcc",
+      cflags: '-std=c99 -mcpu=cortex-m4 -mthumb -mlittle-endian -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Wall -Werror -Wextra -pedantic-errors',
+      cxxflags: '-std=c++03 -mcpu=cortex-m4 -mthumb -mlittle-endian -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Wall -Werror -Wextra -pedantic-errors',
+      ldflags: '-Wl,--entry,ResetISR -Wl,--gc-sections',
+      linker_script: 'src/blinky.ld'
+    )
+
+    app.add_default_tasks('WirelessDebugger:Debug')
+
+end
---
+++ testapp/src/main.c
@@ -0,0 +1,65 @@
+/*  ------------------------------------------------------------
+    Copyright(C) 2015 MindTribe Product Engineering, Inc.
+    All Rights Reserved.
+
+    Author:     Sander Vocke, MindTribe Product Engineering
+                <sander@mindtribe.com>
+
+    Target(s):  ISO/IEC 9899:1999 (target independent)
+    --------------------------------------------------------- */
+
+
+#include <stdio.h>
+
+#include "hw_types.h"
+#include "hw_ints.h"
+#include "hw_memmap.h"
+#include "hw_common_reg.h"
+#include "interrupt.h"
+#include "hw_apps_rcm.h"
+#include "prcm.h"
+#include "rom.h"
+#include "rom_map.h"
+#include "prcm.h"
+#include "gpio.h"
+#include "utils.h"
+
+#include "gpio_if.h"
+#include "pin_mux_config.h"
+
+volatile int doLED = 1;
+
+static int BoardInit(void);
+
+static int BoardInit(void)
+{
+    MAP_IntMasterEnable();
+    MAP_IntEnable(FAULT_SYSTICK);
+    PRCMCC3200MCUInit();
+
+    return 1;
+}
+
+int main(void)
+{
+    BoardInit();
+
+    PinMuxConfig();
+    GPIO_IF_LedConfigure(LED1|LED2|LED3);
+    GPIO_IF_LedOff(MCU_ALL_LED_IND);
+
+    GPIO_IF_LedOn(MCU_GREEN_LED_GPIO);
+
+    unsigned int j = 0;
+    while(1){
+        for(int i=0; i<40000; i++){};
+        if(doLED) GPIO_IF_LedOn(MCU_ORANGE_LED_GPIO);
+        else GPIO_IF_LedOff(MCU_ORANGE_LED_GPIO);
+
+        j++;
+        if(j%100 == 0) GPIO_IF_LedToggle(MCU_RED_LED_GPIO);
+
+    };
+
+    return 1;
+}
