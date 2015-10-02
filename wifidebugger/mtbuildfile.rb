412e30f88237e852d2c63251abe9932f769abc1a
Sander Vocke <sander@mindtribe.com>
2015-10-02 10:09:01 -0700

    added LED behaviour to flash stub for verification.

--- wifidebugger/mtbuildfile.rb
+++ wifidebugger/mtbuildfile.rb
@@ -8,7 +8,7 @@ application_project :wifidebugger, File.dirname(__FILE__) do |app|
       include_paths: ['src', 'src/vendor', 'src/target', 'src/target/cc3200', 'src/common', 'src/interface', 'src/interface/jtag', 
         'src/gdbserver', 'src/thirdparty', 'src/interface/wifi', 'src/interface/uart']
     ),dependencies: [
-      'CC3200_SDK_simplelink:SDK_freertos_multithread_full', 'CC3200_SDK_driverlib:SDK', 'FreeRTOS:Debug', 'CC3200_SDK_inc:SDK', 'CC3200_SDK_oslib:SDK', 'cc3200_flashstub_inc:Protocol', 'cc3200_flashstub:Release'
+      'CC3200_SDK_simplelink:SDK_freertos_multithread_full', 'CC3200_SDK_driverlib:SDK', 'FreeRTOS:Debug', 'CC3200_SDK_inc:SDK', 'CC3200_SDK_oslib:SDK', 'cc3200_flashstub_inc:Protocol', 'cc3200_flashstub:Debug'
     ]
 
 end
