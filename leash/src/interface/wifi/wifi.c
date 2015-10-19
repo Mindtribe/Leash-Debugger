7665ca5185d196f576c52f40bd8a7771c65989a4
Sander Vocke <sander@mindtribe.com>
2015-10-19 11:29:54 -0700

    Added command to report device MAC address.

--- leash/src/interface/wifi/wifi.c
+++ leash/src/interface/wifi/wifi.c
@@ -54,7 +54,8 @@ struct wifi_state_t wifi_state ={
     .status = 0,
     .client_IP = 0,
     .self_IP = 0,
-    .startAP = 0
+    .startAP = 0,
+    .mac = {0}
 };
 
 static int WifiSetModeAP();
@@ -181,21 +182,21 @@ static int WifiDefaultSettings(void)
     //Set mDNS device hostname
     char hostname[64];
     char macstring[20];
-    unsigned char mac[SL_MAC_ADDR_LEN];
     unsigned char maclen = SL_MAC_ADDR_LEN;
     retval = sl_NetCfgGet(SL_MAC_ADDRESS_GET,
             NULL,
             &maclen,
-            mac);
+            wifi_state.mac);
     if(retval < 0) { RETURN_ERROR(retval, "WIFI conf fail"); }
-    snprintf(macstring, 20,  "%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    snprintf(macstring, 20,  "%02X%02X%02X%02X%02X%02X", wifi_state.mac[0],
+            wifi_state.mac[1], wifi_state.mac[2],
+            wifi_state.mac[3], wifi_state.mac[4],
+            wifi_state.mac[5]);
     snprintf(hostname, 64,  "WiFiDebugger%s", macstring);
     retval = sl_NetAppSet (SL_NET_APP_DEVICE_CONFIG_ID,
             NETAPP_SET_GET_DEV_CONF_OPT_DEVICE_URN,
             strlen((const char *)hostname),
             (unsigned char *) hostname);
-    if(retval<0) {RETURN_ERROR(retval, "WIFI hostname fail");}
-
     // Remove  all 64 filters (8*8)
     memset(filter_mask.FilterIdMask, 0xFF, 8);
     retval = sl_WlanRxFilterSet(SL_REMOVE_RX_FILTER, (uint8_t*)&filter_mask,
@@ -208,6 +209,11 @@ static int WifiDefaultSettings(void)
     wifi_state.status = 0;
 
     return RET_SUCCESS;
+}
+
+unsigned char* WifiGetMAC(void)
+{
+    return wifi_state.mac;
 }
 
 static int WifiSetModeAP()
